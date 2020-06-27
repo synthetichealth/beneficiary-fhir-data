46db45ab730a157fb6f606bddeaffd63eebaa267
keithdadkins <keithdadkins@me.com>
2020-06-27 17:21:07 -0500

    Updated keystore build script to work with Public Trust CA signed certificates.

--- ops/ccs-ops-misc/tls-certificates/build-keystores.sh
+++ ops/ccs-ops-misc/tls-certificates/build-keystores.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # ---------------------------------------------------------------------------
-# update-keystores.sh - Installs and verifies TLS certificates in our keystores. Encrypts with ansible-vault.
+# build-keystores.sh - Installs and encrypts BFD Java keystores. This script
+# can be run manually if needed, but should be plugged into CI/CD to ensure
+# our TLS certificates are valid and up to date.
 #
-# Usage: 
-#   update-keystores.sh [-h|--help] [-d|--update-ca-certs] [-e|--environment name] [-s|--source-dir path] <output directory>
-#
+# Usage:
+#     build-keystores.sh -h
 # ---------------------------------------------------------------------------
 PROGNAME=${0##*/}
 
@@ -14,37 +15,34 @@ STORE_PASS="${STORE_PASS:-changeit}"
 KEY_PASS="${KEY_PASS:-changeit}"
 VAULT_PASS="${VAULT_PASS:-}"
 
-# URL's to the CA certs that was used to sign our certificates
-fed_root="https://ocio.nih.gov/Smartcard/Documents/Certificates/Federal_CP_Root_SHA256.cer"
-entrust_root="https://ocio.nih.gov/Smartcard/Documents/Entrust%20Managed%20Services%20Root%20CA.cer"
-intermediate="https://ocio.nih.gov/Smartcard/Documents/HHS-FPKI-Intermediate.cer"
-ca_cert_urls=("$fed_root" "$entrust_root" "$intermediate")
+# URL's to any root/intermediate certificates used to sign our certificates
+root="https://ocio.nih.gov/Smartcard/Documents/Certificates/Entrust_Root_CA-G2.cer"
+intermediate="https://ocio.nih.gov/Smartcard/Documents/Certificates/Entrust_CA-L1k.cer"
+ca_cert_alias=(root intermediate)
+ca_cert_urls=("$root" "$intermediate")
 
-# ca_cert_chain - An array containing the CA root/intermediate certificate chain certificates.
-# The --update-ca-certs option downloads the chain using the url's above. We also convert the chain
-# to PEM format and do some renaming to make them script friendly.
-ca_certs=("federal_cp_root_sha256.pem" "entrust_managed_services_root_ca.pem" "hhs_fpki_intermediate.pem")
-
+# When the --update-ca-certs flag is present, we download the CA chain certificates from nih.gov, and 
+# rename them to be a bit more script friendly.
+ca_certs=("entrust_root_ca_g2.pem" "entrust_ca_l1k.pem")
 
 # program variables
 tmp_dir=$(mktemp -d -t bfd-certs-XXXXXXXXXX)
 dst_dir="${dst_dir:-$(pwd)}"
+dst_dir="${dst_dir%/}" # remove trailing slash
 src_dir=
 environments=()
 default_environments=(prod prod-sbx test)
 downloaded_ca_certs=false
 force=false
 
-
 # Perform pre-exit housekeeping
 clean_up() {
-  rm -rf "$tmp_dir"
+  rm -rf "$tmp_dir" >/dev/null 2>&1
 }
 
 error_exit() {
   echo
   echo -e "$1" >&2
-  clean_up
   exit 1
 }
 
@@ -57,14 +55,13 @@ graceful_exit() {
 signal_exit() {
   case "$1" in
     INT)
-      error_exit "Aborting." ;;
+      error_exit "Bailing out." ;;
     TERM)
       graceful_exit ;;
     *)
       error_exit "$PROGNAME: Terminating on unknown signal" ;;
   esac
 }
-
 
 usage() {
   echo -e "Usage: $PROGNAME -s|--source-dir /path/to/certs/dir [-h|--help] [-u|--update-ca-certs] \
@@ -73,41 +70,44 @@ usage() {
 
 help_message() {
   cat <<- _EOF_
-  $PROGNAME - Exports encrypted BFD keystores with our TLS certificates installed
-  - Installs BFD's CA root, intermediate, and signed TLS certificates into appropriate keystores
-  - Downloads CA root certificate chain if desired
-  - Encrypts with ansible-vault
-  - Exports encrypted keystore to the <destination directory> - defaults to current working directory (.)
-
+  $PROGNAME - Builds and encrypts our BFD Java keystores with the full TLS certificate chain installed.
+  - Downloads CA root certificate chain (if --update-ca-certs is set)
+  - Installs the root CA chain used to sign our certificate
+  - Installs and validates the signed cert
+  - Encrypts and exports the keystore to the <destination directory>
+  
   $(usage)
 
   Examples:
-    # Build all environments keystores (prod, prod-sbx, and test) and export to ~/Desktop.
-    ./$PROGNAME --source-dir /path/to/Keybase/dir ~/Desktop
+    # Build all environments keystores (prod, prod-sbx, and test) and export to ~/Desktop/foo.
+    mkdir -p ~/Desktop/foo
+    ./$PROGNAME --source-dir /path/to/Keybase/dir ~/Desktop/foo
 
-    # Just build nad export the test and prod-sbx keystores and do not prompt to overwrite existing files.
-    ./$PROGNAME --source-dir /path/to/Keybase/dir -e test -e prod-sbx --force ../../ansible/playbooks-ccs/files
+    # Build test and prod-sbx keystores and do not prompt to overwrite any existing files.
+    ./$PROGNAME --source-dir /path/to/Keybase/dir -e test -e prod-sbx --force ~/Desktop/foo
 
-    # Download the CA root chain from the internet and then build and export all to the current working directory.
-    ./$PROGNAME --source-dir /path/to/Keybase/dir --update-ca-certs
+    # Download CA certs from the internet and export keystores in preparation for a deployment.
+    ./$PROGNAME --update-ca-certs --source-dir /Volumes/Keybase/path/to/bfd/keys ../../ansible/playbooks-ccs/files
 
   Options:
   -h, --help  Display this help message and exit.
-  -s, --source-dir path  [Required] Source directory containing our keystores and signed certs.
-    Where 'path' is the Path to source directory. This could be /Volumes/Keybase/....
+  -s, --source-dir path  [Required] Source directory containing our pristine keystores and signed certs.
+    Where 'path' is the Path to source directory. This could be /Volumes/Keybase/.... And 'pristine' meaning that 
+    the source keystores only contain the private key
   [-u, --update-ca-certs]  Download root and intermediate CA certificates.
-  [-e, --environment name]  Specify one or more environment(s) to process. I.e. '-e prod -e test'.
-    Where 'name' is the Name of the environment. E.g., prod prod-sbx or test. Defaults to all.
-  [-f, --force] Do not prompt for user input. Warning! This may overwrite existing files.
+  [-e, --environment name]  Specify one or more environment(s) to process by setting one or more '-e' flags.
+    Where 'name' is the Name of an environment. E.g., '-e prod -e prod-sbx'. Defaults to all environments.
+  [-f, --force] Do not prompt for user input. This will overwrite any existing files.
 
 _EOF_
   return
 }
 
 
-# ensures we have openssl and keytool installed and makes sure we are not using the default MacOS openssl
+# ensures we have 'openssl' and 'keytool' installed. Also verifies we are not using the default MacOS openssl
 check_tools(){
   printf "Checking tools..."
+  
   # keytool
   (command -v keytool >/dev/null 2>&1) || error_exit "Missing 'keytool' command. Please install Java."
 
@@ -126,7 +126,6 @@ check_tools(){
   # ansible-vault
   (command -v ansible-vault >/dev/null 2>&1) || error_exit "Missing 'ansible-vault' command. Please install ansible."
 
-  
   echo " OK"
 }
 
@@ -145,7 +144,7 @@ check_files(){
     done
   fi
 
-  # CA certs should all be in tmp_dir by now.. verify
+  # CA certs should all be in the $tmp_dir by now.. verify we have them all
   for ca_cert in "${ca_certs[@]}"; do
     [ ! -f "$tmp_dir"/"$ca_cert" ] && missing_files+=("$ca_cert")
   done
@@ -154,7 +153,7 @@ check_files(){
     for f in "${missing_files[@]}"; do
       echo "  - $f"
     done
-    echo "Maybe run with '--update-ca-certs' option to download them."
+    echo "Maybe run with '--update-ca-certs' option to download them first."
     error_exit "Exiting."
   fi
   
@@ -165,7 +164,7 @@ check_files(){
     [ ! -f "${src_dir}/${environment}.bfd.cms.gov.pem" ] && missing_files+=("${environment}.bfd.cms.gov.pem")
   done
   if [[ ${#missing_files[@]} -gt 0 ]]; then
-    echo "Missing keystores or certificates in the source directory:"
+    echo "Missing keystores and/or signed certificates in $src_dir:"
     for f in "${missing_files[@]}"; do
       echo "  - $f"
     done
@@ -184,7 +183,7 @@ fetch_certs(){
   if [[ "$force" == "false" ]] && [[ "$ca_exists" == "true" ]]; then
     read -r -p "Overwrite existing CA certs in source directory? [y/n] " response
     if [[ ! "$response" == "y" ]]; then
-      error_exit "Aborting."
+      error_exit "Bailing out."
     fi
   fi
 
@@ -221,18 +220,17 @@ fetch_certs(){
   echo " OK"
 }
 
-# import certs, export, and encrypt
+# import root chain
 import_root_chain(){
   local keystore="$1"
   printf "Importing root chain"
   (
     cd "$tmp_dir" || error_exit "Could not cd into $tmp_dir"
-    
-    # import the CA chain in order
+    local cert_index=0
     for ca_cert in "${ca_certs[@]}"; do
-      keytool -importcert -noprompt -alias "$ca_cert" -trustcacerts -keystore "$keystore" -storepass "$STORE_PASS" -file "$ca_cert" 2>/dev/null
+      keytool -importcert -noprompt -alias "${ca_cert_alias[cert_index]}" -trustcacerts -keystore "$keystore" -storepass "$STORE_PASS" -file "$ca_cert" 2>/dev/null
       printf "."
-      rm "$ca_cert"
+      cert_index+=1
     done
   )
   echo " OK"
@@ -242,7 +240,7 @@ import_signed_cert(){
   local environment="$1"
   local keystore="$2"
   local signed_cert="$3"
-  printf "Importing %s's signed cert... " "$environment"
+  printf "Importing signed cert... "
   (
     cd "$tmp_dir" || error_exit "Could not cd into $tmp_dir"
     if (keytool -import -alias server -trustcacerts -keystore "$keystore" -storepass "$STORE_PASS" -file "$signed_cert" 2>/dev/null); then
@@ -254,6 +252,48 @@ import_signed_cert(){
       error_exit "Failed to import $signed_cert into $keystore. Exiting."
     fi
     rm "$signed_cert"
+  )
+}
+
+# TODO: remove this.
+# Import entries from our old keystores to handle the old domains until everyone is moved over.
+# Once everyone is moved over, we can simply remove this from the build process.
+import_old(){
+  (
+    cd "$tmp_dir" || error_exit "Could not cd into $tmp_dir"
+    local environment="$1"
+    local keystore="$2"
+    local partners=(mct dpc bcda bb ab2d)
+
+    printf "Generating self-signed certs for bfdcloud domain..."
+    # build a list of our subject alternate names to pass as -ext arg to keytool
+    ext="san=dns:${environment}.bfdcloud.net"
+
+    # partner domains
+    for partner in "${partners[@]}"; do
+      ext+=",dns:${partner}.${environment}.bfdcloud.net"
+    done
+
+    # not sure if these are still in use, adding them because they were in the existing certs
+    case "$environment" in
+      prod)
+        ext+=",dns:internal-pdcw10lb01-1951212262.us-east-1.elb.amazonaws.com"
+      ;;
+      prod-sbx)
+        ext+=",dns:internal-dpcwelb01-2074070868.us-east-1.elb.amazonaws.com"
+        ext+=",dns:fhir.backend.bluebutton.hhsdevcloud.us"
+      ;;
+      test)
+        ext+=",dns:internal-tsbb10lb01-758855236.us-east-1.elb.amazonaws.com"
+      ;;
+    esac
+    
+    # generate the self-signed certs and add to our keystore under the "self-signed" alias
+    (keytool -genkeypair -alias "self-signed" -keyalg RSA -keysize 4096 \
+        -dname "cn=${environment}.bfdcloud.net" \
+        -ext "$ext" \
+        -validity 730 -keypass changeit -keystore "${environment}_bfd_cms_gov.jks" -storepass changeit 2>/dev/null)
+    echo " OK"
   )
 }
 
@@ -270,14 +310,53 @@ get_vault_pass(){
   echo " OK"
 }
 
+
+export_public_certs(){
+  local environment="$1"
+  local keystore="$2"
+  (
+    cd "$tmp_dir" || error_exit "Could not cd into $tmp_dir"
+    
+    # extract *.bfd.cms.gov public cert
+    (keytool -export -keystore "${keystore}" -alias "server" -storepass changeit -file "${environment}.bfd.cms.gov.public.pem" -rfc 2>/dev/null)
+    local dst_file="${dst_dir}/${environment}.bfd.cms.gov.public.pem"
+    if [[ -f $dst_file ]]; then
+      read -r -p "Overwrite ${environment}.bfd.cms.gov.public.pem? [y/n] " response
+      if [[ "$response" == "y" ]]; then
+        cp -f  "${environment}.bfd.cms.gov.public.pem" "$dst_file"
+      else
+        error_exit "Bailing out."
+      fi
+    else
+      cp "${environment}.bfd.cms.gov.public.pem" "$dst_file"
+    fi
+
+    # extract the bfddcloud self-signed public cert
+    local dst_file="${dst_dir}/${environment}.bfdcloud.net.public.pem"
+    (keytool -export -keystore "${keystore}" -alias "self-signed" -storepass changeit -file "${environment}.bfdcloud.net.public.pem" -rfc 2>/dev/null)
+    if [[ -f $dst_file ]]; then
+      read -r -p "Overwrite ${environment}.bfdcloud.net.public.pem? [y/n] " response
+      if [[ "$response" == "y" ]]; then
+        cp -f  "${environment}.bfdcloud.net.public.pem" "$dst_file"
+      else
+        error_exit "Bailing out."
+      fi
+    else
+      cp "${environment}.bfdcloud.net.public.pem" "$dst_file"
+    fi
+  )
+}
+
 encrypt_and_export_keystore(){
   local keystore="$1"
   (
     cd "$tmp_dir" || error_exit "Could not cd into $tmp_dir"
-    echo "Exporting $keystore to $dst_dir..."
-    ansible-vault encrypt "$keystore" --vault-password-file <(echo "$VAULT_PASS")
-    
+    printf "Encrypting keystore..."
+    (ansible-vault encrypt "$keystore" --vault-password-file <(echo "$VAULT_PASS") >/dev/null 2>&1 )
+    echo " OK"
+
     # export.. make sure they want to overwrite if keystore exists
+    echo "Exporting $keystore"
     if [[ "$force" == "true" ]]; then
       cp -f "$keystore" "$dst_dir"/"$keystore"
     else
@@ -286,10 +365,13 @@ encrypt_and_export_keystore(){
         if [[ "$response" == "y" ]]; then
           cp -f "$keystore" "$dst_dir"/"$keystore"
         else
-          error_exit "Aborting."
+          error_exit "Bailing out."
         fi
+      else
+        cp "$keystore" "$dst_dir"/"$keystore"
       fi
     fi
+    echo "Successfully built, encrypted and exported $keystore to $dst_dir"
   )
 }
 
@@ -331,30 +413,42 @@ while [[ -n $1 ]]; do
   shift
 done
 
+# --- MAIN PROGRAM LOGIC BELOW ---
+check_tools
+get_vault_pass
+
+# set environments and prepare files
 if [[ ${#environments[@]} -eq 0 ]]; then
   environments=("${default_environments[@]}")
 fi
 
-check_tools
-
-# download CA root and intermediate certs
+# download CA root and intermediate certs if selected
 if [[ "$download_certs" == "true" ]]; then
   fetch_certs
 fi
-
-get_vault_pass
 check_files
 
-# ready to import
+# import, build, encrypt, and export the keystore
 for e in "${environments[@]}"; do
-  # copy our pristine keystores and signed tls cert to tmp
+  echo
+  echo "---- BUILDING ${e^^} KEYSTORE----"
+  # copy our keystores and signed certs to tmp
   cp "$src_dir/${e}_bfd_cms_gov.jks" "$tmp_dir"
   cp "$src_dir/${e}.bfd.cms.gov.pem" "$tmp_dir"
 
-  # import root chain, certs, encrypt, and export
-  import_root_chain "${e}_bfd_cms_gov.jks"
-  import_signed_cert "$e" "${e}_bfd_cms_gov.jks" "${e}.bfd.cms.gov.pem"
-  encrypt_and_export_keystore "${e}_bfd_cms_gov.jks"
+  # import our CA root chain into the keystore
+  import_root_chain "${e}_bfd_cms_gov.jks" || break
+
+  # Import the signed cert
+  import_signed_cert "$e" "${e}_bfd_cms_gov.jks" "${e}.bfd.cms.gov.pem" || break
+
+  # import_old "$e" "${e}_bfd_cms_gov.jks"
+
+  # export public certs
+  # export_public_certs "$e" "${e}_bfd_cms_gov.jks"
+  
+  # encrypt and export
+  encrypt_and_export_keystore "${e}_bfd_cms_gov.jks" || break
 done
 
 graceful_exit
