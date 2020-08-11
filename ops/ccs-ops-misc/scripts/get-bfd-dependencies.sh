e7f60309ebf7f92c92b9b9cbfaeff38db5bf9ff1
keithdadkins <keithdadkins@me.com>
2020-08-11 15:59:07 -0500

    Bug fix and typo

--- ops/ccs-ops-misc/scripts/get-bfd-dependencies.sh
+++ ops/ccs-ops-misc/scripts/get-bfd-dependencies.sh
@@ -4,7 +4,7 @@ trap 'usage' ERR
 
 usage() {
   echo "Please provide the name or id of the running bfd app container. Ex:"
-  echo "    './gget-bfd-dependencies.sh contributing_bfd_1'"
+  echo "    './get-bfd-dependencies.sh contributing_bfd_1'"
   echo "You can run 'docker ps' to list the running containers on your system."
   echo ""
   echo "To save the output to a file:"
@@ -14,7 +14,7 @@ usage() {
 [[ "$1" =~ ^help.* ]] && usage && exit 0
     
 mvncmd='mvn dependency:list | grep "\[INFO\][^:]*:[^:]*:[^:]*:[^:]*:.*" | cut -d] -f2- | sort | uniq > bfd.depends.txt'
-if docker exec "$1" /bin/bash -c "cd /app/apps; ${mvncmd} > bfd.depends.txt"; then
+if docker exec "$1" /bin/bash -c "cd /app/apps; ${mvncmd}"; then
   docker exec "$1" /bin/bash -c "cat /app/apps/bfd.depends.txt"
 else
   usage
